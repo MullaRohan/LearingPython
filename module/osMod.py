04e4dbcb43c9bc20177512c2d08134a0aa30c587
MullaRohan <mdrohanislam444@gmail.com>
2025-08-28 11:14:52 +0600

    osMod added

--- module/osMod.py
+++ module/osMod.py
@@ -1,3 +1,22 @@
 import os as s
 
+
+# current directory
 print(s.getcwd())
+#change directory
+s.chdir(f"E:\\XPSC Level-1")
+print(s.getcwd())
+
+#make a folder, 
+directory = "RKTesting"
+paths = "E:/XPSC Level-/"
+di = s.path.join(paths,directory)
+s.makedirs(di, exist_ok=True)
+print(s.getcwd())
+print(s.listdir())
+
+#macking a file
+name = "hellow.py"
+paths = "E:/XPSC Level-/"
+di = s.path.join(paths,name)
+s.makedirs(di)
