5e1ff09d202e7ade70db5507b819cc776585cce7
MullaRohan <mdrohanislam444@gmail.com>
2025-08-28 11:31:54 +0600

    numpy installed

---
+++ startNumpy/nmpy1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import random as rn
+
+print(np.abs(-100))
+print(np.sqrt(49))
+
+list1 = ["I Love You", "I Hate You", "I Like You"]
+
+# print(np.random.choices())
+print(np.random.choice(list1))
+# print(rn.choices(lis1,[10,5,1]))
