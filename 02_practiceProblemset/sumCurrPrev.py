f4366087791db4f71fc5a0f18448dcf184ec4406
MullaRohan <mdrohanislam444@gmail.com>
2025-09-05 12:10:24 +0600

    updated

---
+++ 02_practiceProblemset/sumCurrPrev.py
@@ -0,0 +1,11 @@
+# print(f"Current Number is 0, Prev Number was 0 sum = 0")
+# for i in range(0, 11, 1):
+#     print(f"Current Number is {i+1}, Prev Number was {i} sum = {i+i+1}")
+
+
+# print Character in even index
+
+# str = input("Enter original String: ")
+
+# for i in range(0, len(str), 2):
+#     print(str[i])
